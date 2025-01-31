2364976388c99c58fc81b890b999f20ddca9a02b
Tanishq Nigam <tnigam27@gmail.com>
2025-01-31 18:08:57 +0530

    Revert "delete"
    This reverts commit 7725e8b8004be6af088f4e99a2e7a8b4c4bf022b.

--- backend/index.ts
+++ backend/index.ts
@@ -1,46 +1,24 @@
-import express, { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import fs from 'fs';
-import { getStockData } from './service';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-// Initialize the express app
-const app = express();
-const port = 5000;
+import main from "./main";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
 /////uayuay
-// Middleware to handle JSON requests
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Serve static files (e.g., images, CSS, JS files) if needed
-app.use(express.static(path.join(__dirname, 'public')));
-
-// API endpoint to fetch stock data
-app.get('/api/stock/:symbol', (req: Request, res: Response, next: NextFunction) => {
-  const { symbol } = req.params;
-  try {
-    const data = getStockData(symbol);
-    if (data) {
-      res.json(data); // Send the stock data as a JSON response
-    } else {
-      res.status(404).json({ message: 'Stock data not found' }); // Handle 404 if stock data is not found
-    }
-  } catch (error) {
-    next(error); // Pass error to the error handler middleware
-  }
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the alloan.ai");
 });
 
-// Default route (for testing)
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the stock API!');
-});
+app.use("/api", main);
 
-// Error handling middleware (to catch unexpected errors)
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
-});
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Backend server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
